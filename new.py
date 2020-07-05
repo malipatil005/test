42d13c5dc71bef84e2259b76b7480299a58aced3
chandru <chandu.patil005@gmail.comgit config --global user.name chandrugit config --global user.email chandu.patil005@gmail.com>
2020-07-05 09:54:00 +0530

    added childrens

--- new.py
+++ new.py
@@ -2,3 +2,6 @@ print("what is your good name\n")
 print("my good name is chandru\n")
 print("what is ur wife name\n")
 print("my wife name is saru\n")
+print("how many kids you will have")
+print("i will have two kids")
+
